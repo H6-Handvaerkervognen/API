7d0d86a6d7cf9bb5027f04dcc59b8345961e323a
NansyT <nanna.tronbjerg@gmail.com>
2023-04-26 08:30:14 +0200

    Update loginManager.cs

--- HåndværkervognenAPI/HåndværkervognenAPI/Managers/loginManager.cs
+++ HåndværkervognenAPI/HåndværkervognenAPI/Managers/loginManager.cs
@@ -7,23 +7,23 @@ namespace HåndværkervognenAPI.Managers
 {
     public class loginManager : ILoginService
     {
-        private IDatabase database;
-        private IHashing hashing;
+        private IDatabase _database;
+        private IHashing _hashing;
 
 
 
         public loginManager(IDatabase database, IHashing hashing)
         {
-            this.database = database;
-            this.hashing = hashing;
+            _database = database;
+            _hashing = hashing;
         }
 
       
 
         public bool AuthorizeLogin(LoginCredentials loginCredentials)
         {
-            UserDal user = database.GetUser(loginCredentials.Username);
-            byte[] hashPassword = hashing.GenerateHash(loginCredentials.Password, user.Salt);
+            UserDal user = _database.GetUser(loginCredentials.Username);
+            byte[] hashPassword = _hashing.GenerateHash(loginCredentials.Password, user.Salt);
 
             if (Encoding.ASCII.GetString(hashPassword) == user.HashPassword)
             {
@@ -39,10 +39,10 @@ namespace HåndværkervognenAPI.Managers
 
         public void RegisterUser(LoginCredentials loginCredentials)
         {
-            var salt = hashing.GenerateSalt();
-            var hashPassword = hashing.GenerateHash(loginCredentials.Password, salt);
+            var salt = _hashing.GenerateSalt();
+            var hashPassword = _hashing.GenerateHash(loginCredentials.Password, salt);
             UserDal user = new UserDal(loginCredentials.Username, Encoding.ASCII.GetString(hashPassword), salt);
-            database.CreateUser(user);
+            _database.CreateUser(user);
         }
     }
 }
